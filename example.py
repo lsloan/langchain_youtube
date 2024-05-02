0183c64702b8078960ffee8f29cc025c4a32c041
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2024-05-02 10:00:44 -0400

    #2 - include an embed-style URL for testing

--- example.py
+++ example.py
@@ -1,7 +1,8 @@
 from LangChainYouTube import YouTubeCaptionLoader
 
 # U2 - When Love Comes To Town
-mediaUrl = 'https://www.youtube.com/watch?v=7mCE73j59Zs'
+# mediaUrl = 'https://www.youtube.com/watch?v=7mCE73j59Zs'  # watch-style URL
+mediaUrl = 'https://www.youtube.com/embed/7mCE73j59Zs'  # embed-style URL
 
 captionLoader = YouTubeCaptionLoader(mediaUrl)
 
